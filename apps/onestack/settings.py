d752eebb0ed2c4c56b2abc567472b6cd8f4df7b4
张志亮 <zhilight@gmail.com>
2017-07-05 01:56:26 +0800

    安装apps

--- apps/onestack/settings.py
+++ apps/onestack/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jumpserver',
+    'audits',
+    'api',
+    'assets',
+    'deploy',
+    'monitor',
+    'wikis',
 ]
 
 MIDDLEWARE = [
