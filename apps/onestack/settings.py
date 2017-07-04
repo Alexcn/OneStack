b94c9189a8fb0eb2608df8f09cfb50994b02b709
张志亮 <zhilight@gmail.com>
2017-07-05 01:51:37 +0800

    适配postgres数据库

--- apps/onestack/settings.py
+++ apps/onestack/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'onestack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onestack',
+        'USER': 'one',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
