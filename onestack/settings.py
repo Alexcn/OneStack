aca2ca6899aadeab1a6241ae55ad961f860b7ab9
张志亮 <zhilight@gmail.com>
2017-07-07 00:22:54 +0800

    模板配置

--- onestack/settings.py
+++ onestack/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'onestack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
