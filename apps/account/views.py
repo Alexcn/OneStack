5193441402b6afb867ca88dff93187be1ba8bb5e
张志亮 <zhilight@gmail.com>
2017-07-07 18:02:21 +0800

    添加表单校验

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 
@@ -10,16 +13,28 @@ from django.views.decorators.csrf import csrf_protect
 
 
 class DashBoardView(View):
-    @method_decorator(login_required)
+    @method_decorator(login_required(login_url='account/login'))
     def get(self, request):
-        pass
+        return render(request, 'account/dashboard.html')
 
 
 class LoginView(View):
     @method_decorator(csrf_protect)
     @method_decorator(never_cache)
     def post(self, request):
-        pass
+        msg = []
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                auth_login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                msg.append('Disabled account!')
+        else:
+            msg.append('Password 错误！')
+        return render(request, 'account/login.html', {'errors': msg})
 
     def get(self, request):
         return render(request, 'account/login.html')
