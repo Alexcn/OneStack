b4c59673543c7f7f6c2a8c197bcc34fe5e03450a
张志亮 <zhilight@gmail.com>
2017-07-11 14:48:50 +0800

    添加通用模型

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='修改时间')
+
+    class Meta:
+        abstract = True
