583123771f2b8f42686f3b6acc925aa023da5d97
张志亮 <zhilight@gmail.com>
2017-07-19 00:13:26 +0800

    定义视图的基本结构

--- apps/assets/urls.py
+++ apps/assets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-
+from .views import AssetListView
 
 urlpatterns = [
-    # url(r'^$', DashBoardView.as_view(), name='dashboard'),
+    url(r'^list/$', AssetListView.as_view(), name='dashboard'),
+    url(r'^add/$', AssetListView.as_view(), name='dashboard'),
 ]
--- apps/assets/views.py
+++ apps/assets/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class AssetListView(TemplateView):
-    pass
+    @method_decorator(login_required(login_url='account/login'))
+    def get(self, request):
+        pass
+
+
+class AssetAddView(TemplateView):
+    @method_decorator(login_required(login_url='account/login'))
+    def get(self, request):
+        pass
+
+    @method_decorator(login_required(login_url='account/login'))
+    def post(self, request):
+        pass
