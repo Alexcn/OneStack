467f518993ea11085cc8514e451ba63d7df4843e
张志亮 <zhilight@gmail.com>
2017-07-25 16:22:47 +0800

    add new table

--- apps/wikis/models.py
+++ apps/wikis/models.py
@@ -4,12 +4,28 @@ from model_utils.fields import StatusField
 # from model_utils import Choices
 
 
+class Categories(BaseModel):
+    name = models.CharField(max_length=32, unique=True)
+
+    def categories_article_count(self):
+        return self.wiki_set.all().count()
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=32, unique=True)
+
+    def tag_article_count(self):
+        return self.wiki_set.all().count()
+
+
 class Wiki(BaseModel):
     title = models.CharField(max_length=254, db_index=True)
     # author_id = models.IntegerField(default=1)
     content = models.TextField(max_length=5000)
     # STATUS = Choices('draft', 'published')
     # status = StatusField()
+    # categories = models.ManyToManyField(Categories)
+    # tag = models.ManyToManyField(Tag)
 
     class Meta:
         db_table = 'wikis'
