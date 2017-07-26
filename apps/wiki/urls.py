679422e557b7d77dcf71aebd6996ff2e4a49e830
张志亮 <zhilight@gmail.com>
2017-07-26 15:42:11 +0800

    完成wiki列表页面

--- apps/wiki/urls.py
+++ apps/wiki/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
-from .views import WikiListView
+from .views import WikiListView, WikiAddView, WikiDetailView, WikiEditView
 
 urlpatterns = [
     url(r'^list/$', WikiListView.as_view(), name='wiki_list'),
+    url(r'^add/$', WikiAddView.as_view(), name='wiki_add'),
+    url(r'^detail/$', WikiDetailView.as_view(), name='wiki_detail'),
+    url(r'^edit/$', WikiEditView.as_view(), name='wiki_edit'),
 ]
