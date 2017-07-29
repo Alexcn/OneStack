30e559ac52afff4431a052640a468d27a8bda061
张志亮 <zhilight@gmail.com>
2017-07-29 15:49:57 +0800

    修改资产视图

--- apps/assets/views.py
+++ apps/assets/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
+from apps.common.utils import LoginRequiredMixin
 
 
-class AssetListView(TemplateView):
-    @method_decorator(login_required(login_url='account/login'))
+class AssetListView(LoginRequiredMixin, TemplateView):
     def get(self, request):
         pass
 
 
-class AssetAddView(TemplateView):
-    @method_decorator(login_required(login_url='account/login'))
+class AssetAddView(LoginRequiredMixin, TemplateView):
     def get(self, request):
         pass
 
-    @method_decorator(login_required(login_url='account/login'))
     def post(self, request):
         pass
