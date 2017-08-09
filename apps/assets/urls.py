b293bbcc4b8fe902068ffdaebc91819e3da344eb
张志亮 <zhilight@gmail.com>
2017-08-09 12:15:49 +0800

    修改视图路径

--- apps/assets/urls.py
+++ apps/assets/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # url(r'^list/$', AssetListView.as_view(), name='dashboard'),
     url(r'^hardware/add/$', HardwareAssetAddView.as_view(), name='hardware_add'),
     url(r'^hardware/update/(?P<pk>[0-9]+)/$', HardwareAssetUpdateView.as_view(), name='hardware_update'),
-    url(r'^hardware/list/$', HardwareAssetListView.as_view(), name='hardware_list'),
+    url(r'^hardware/$', HardwareAssetListView.as_view(), name='hardware_list'),
 ]
