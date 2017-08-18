03e7bc6ae6728ff250c9e8280489780afeee4709
张志亮 <zhilight@gmail.com>
2017-08-19 02:08:24 +0800

    fix

--- onestack/settings.py
+++ onestack/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
