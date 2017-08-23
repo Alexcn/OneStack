0e6895376165d4bd61ef446ef52662e6bd2445ce
张志亮 <zhilight@gmail.com>
2017-08-23 20:21:56 +0800

    建立新的用户认证模型

--- apps/account/models.py
+++ apps/account/models.py
@@ -7,5 +7,20 @@ from django.contrib.auth.models import AbstractUser, Group, Permission, Permissi
 from django.core.validators import RegexValidator
 
 
-# class User(AbstractUser):
-#     pass
+class AuthSource(models.Model):
+    class Meta:
+        db_table = 'auth_sources'
+
+    auth_type = models.CharField(max_length=30)
+    name = models.CharField(max_length=60)
+    host = models.CharField(max_length=120, default='')
+    port = models.IntegerField(default=None)
+    account = models.CharField(max_length=60, default='')
+    account_password = models.CharField(max_length=255, default='')
+    base_dn = models.CharField(max_length=255, default=None)
+    tls = models.BooleanField(default=False)
+    timeout = models.IntegerField(default=5)
+
+
+class User(AbstractUser):
+    auth_source = models.ForeignKey(AuthSource, default=None)
